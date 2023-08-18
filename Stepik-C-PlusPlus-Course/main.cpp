954f38545eb1cc3260fbed8e47dc77066f89cf8f
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-08-18 21:00:41 +0400

    Initial Commit

---
+++ Stepik-C-PlusPlus-Course/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Stepik-C-PlusPlus-Course
+//
+//  Created by Максим Митрофанов on 18.08.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
