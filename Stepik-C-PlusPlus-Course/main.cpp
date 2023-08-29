efbbd4769c4a521ef65fcaddcb5082d3d37f2ffc
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-08-29 14:19:31 +0400

    Finished matrix transposition

--- Stepik-C-PlusPlus-Course/main.cpp
+++ Stepik-C-PlusPlus-Course/main.cpp
@@ -22,38 +22,34 @@ void printMatrix(int (&matrix)[ColumnCount][RowCount], unsigned rows, unsigned c
 
 template<std::size_t ColumnCount, std::size_t RowCount >
 int ** transposeCustomMatrix(int (&matrix)[ColumnCount][RowCount], unsigned rows, unsigned cols) {
-    int lastValueIndex = 0;
-    int** resultingMatrix = new int* [rows];
-    resultingMatrix[0] = new int [rows * cols];
+    int ** transposedMatrix = new int* [cols];
     
-    for (size_t columnIndex = 0; columnIndex < cols; ++columnIndex) {
-        for (int rowIndex = 0; rowIndex < rows; ++rowIndex) {
-            resultingMatrix[0][lastValueIndex] = matrix[rowIndex][columnIndex];
-            lastValueIndex += 1;
+    for (int rowIndex = 0; rowIndex < cols; ++rowIndex) {
+        transposedMatrix[rowIndex] = new int [rows];
+    }
+    
+    for (size_t rowIndex = 0; rowIndex < cols; ++rowIndex) {
+        for (size_t columnIndex = 0; columnIndex < rows; ++columnIndex) {
+            transposedMatrix[rowIndex][columnIndex] = matrix[columnIndex][rowIndex];
         }
     }
     
-    for (size_t rowIndex = 1; rowIndex < cols; ++rowIndex) {
-        resultingMatrix[rowIndex] = resultingMatrix[rowIndex - 1] + rows;
-    }
-    
-    // Result in console
     for (size_t rowIndex = 0; rowIndex < cols; ++rowIndex) {
-        for (int columnIndex = 0; columnIndex < rows; ++columnIndex) {
-            cout << resultingMatrix[rowIndex][columnIndex] << " ";
+        for (size_t columnIndex = 0; columnIndex < rows; ++columnIndex) {
+            cout << transposedMatrix[rowIndex][columnIndex] << " ";
         }
         cout << endl;
     }
     cout << endl;
     
-    return resultingMatrix;
+    return transposedMatrix;
 }
 
 
 int main(int argc, const char * argv[]) {
-    int rowsCount = 4;
+    int rowsCount = 5;
     int columnsCount = 3;
-    int originalMatrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {11, 12, 13}};
+    int originalMatrix[5][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {11, 12, 13}, {21, 22, 23}};
     
     cout << "Original matrix:" << endl;
     printMatrix(originalMatrix, rowsCount, columnsCount);
